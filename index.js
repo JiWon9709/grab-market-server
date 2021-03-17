19244290962e0fe8c1f8bc37d4260c1cf016d642
jiwon <ji_ji1997@naver.com>
2021-03-17 14:22:25 +0900

    heroku cloud

--- index.js
+++ index.js
@@ -1,30 +1,113 @@
-var http = require("http");
-var hostname = "127.0.0.1"; // 내 컴퓨터
-var port = 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const product = require("./models/product");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+const port = process.env.PORT || 8080;
 
-const server = http.createServer(function (req, res) {
-  //   console.log("request : ", req);
-  //   res.end("hello client");
-  const path = req.url;
-  const method = req.method;
-  if (path === "/products") {
-    if (method === "GET") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      const products = JSON.stringify([
-        {
-          name: "농구공",
-          price: 5000,
-        },
-      ]);
-      res.end(products);
-    } else if (method === "POST") {
-      res.end("생성 되었습니다!");
-    }
-  } else {
-    res.end("good bye");
-  }
+app.use(express.json());
+app.use(cors()); // 모든 브라우저에서 사용가능
+app.use("/uploads", express.static("uploads"));
+
+app.get("/products", (req, res) => {
+  // const query = req.query;
+  // console.log("query: ", query);
+  models.Product.findAll({
+    // limit: 1,
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  })
+    .then((result) => {
+      console.log("products : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러 발생");
+    });
 });
 
-server.listen(port, hostname);
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+    imageUrl,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과:", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
+    });
+  // res.send({
+  //   body: body, // 키와 value 가 같으면 body만 써도됨
+  // });
+});
 
-console.log("grab market server on!");
+// 동적으로 경로 지정
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  // res.send(`id는 ${id}입니다`);
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("product : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품조회에 에러가 발생했습니다.");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("db 연결 성공");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("db 연결 에러");
+      process.exit();
+    });
+});
